11442a9759e4a90df772afe5a28ace9eae2fbd4d
Vikas Rana <vikas.rana@cloudsmartz.net>
2017-05-16 10:55:51 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -11,14 +11,12 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./server/config'); // get our config file
 var cors = require('cors');
 var http = require('http');
-
+var path = require('path');
 // load models
 var models = require('./server/models');
 
 app.set('superSecret', config.secret); // secret variable
 var port = process.env.PORT || 8090; // set our port
-
-var ignoreJWTRoutes = ['authenticate'];
 
 var whitelist = ['http://localhost:8080']
 var corsOptions = {
@@ -33,53 +31,23 @@ app.use(cors(corsOptions));
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('view engine', 'ejs');
+
+app.get('*', function(req, res){
+    res.status(404).json({
+		code: 404,
+		success: false,
+		message: 'Not Found',
+		payload: {
+			/* Application-specific data would go here. */
+	    }
+	});
+});
 // parse application/vnd.api+json as json
 //app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
-app.use(function(req, res, next) {
-
-    var parts = req.path.split('/');
-    currentPath = parts[parts.length - 1];
-
-    if (ignoreJWTRoutes.indexOf(currentPath) === -1 && req.method.toLowerCase() != 'options') {
-        // check header or url parameters or post parameters for token
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            token = req.headers.authorization.split(' ')[1];
-        } else if (req.query && req.query.token) {
-            token = req.query.token;
-        } else {
-            token = req.body.token || req.query.token || req.headers['x-access-token'];
-        }
-        // decode token
-        if (token && typeof token != undefined) {
-            // verifies secret and checks exp
-            jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-                if (err) {
-                    return res.status(401).send({
-                        'success': false,
-                        'error-message': 'Unclassified Authentication Failure',
-                        'error-auxiliary': 'Access token is invalid. Please try with new Access token'
-                    });
-                } else {
-                    // if everything is good, save to request for use in other routes
-                    req.decoded = decoded;
-                    next();
-                }
-            });
-        } else {
-            // if there is no token
-            // return an error
-            return res.status(403).send({
-                'success': false,
-                'error-message': 'Unclassified Authentication Failure',
-                'error-auxiliary': 'Missing authorization token. Please try with valid Access token.'
-            });
-        }
-    } else {
-        // Pass to next layer of middleware
-        next();
-    }
-});
 var router = express.Router(); // get an instance of the express Router
 // all of our routes will be prefixed with /api
 app.use('/api', router);
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,50 @@ var _AUTH = require('./auth');
 module.exports = function(app,router,jwt) {
 	
 	/* Auth */
-	router.post('/authenticate', _AUTH.authenticate(app,jwt));
+	router.post('/authenticate', _AUTH.authenticate(app, jwt));
 	/* End Auth */
+	
 	/* User */
-    router.get('/users', _AUTH.getUsers);
-	router.get('/users/:id', _AUTH.getUserById);
+    router.get('/users', tokenProtection, _AUTH.getUsers);
+	router.get('/users/:id', tokenProtection, _AUTH.getUserById);
     /* End User */
 	router.get('*', function(req, res, next) {
 		res.render('app/index', { page_title: 'PEN_V4', user_data: {} });
 	})
 }
+
+function tokenProtection(req, res, next){
+	// check header or url parameters or post parameters for token
+	if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+		token = req.headers.authorization.split(' ')[1];
+	} else if (req.query && req.query.token) {
+		token = req.query.token;
+	} else {
+		token = req.body.token || req.query.token || req.headers['x-access-token'];
+	}
+	// decode token
+	if (token && typeof token != undefined) {
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+			if (err) {
+				return res.status(401).send({
+					'success': false,
+					'error-message': 'Unclassified Authentication Failure',
+					'error-auxiliary': 'Access token is invalid. Please try with new Access token'
+				});
+			} else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;
+				next();
+			}
+		});
+	} else {
+		// if there is no token
+		// return an error
+		return res.status(403).send({
+			'success': false,
+			'error-message': 'Unclassified Authentication Failure',
+			'error-auxiliary': 'Missing authorization token. Please try with valid Access token.'
+		});
+	}
+}	
